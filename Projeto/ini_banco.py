b9b1eb0e3b6b93c1488fcb0827dd4dacbc13a6d4
AnnaCSantos <annabeatrizsantos@alunos.utfpr.edu.br>
2026-06-19 22:54:05 -0300

    Cadastro de avaliações teste

--- Projeto/ini_banco.py
+++ Projeto/ini_banco.py
@@ -1,5 +1,5 @@
 # seed.py
-from banco import criar_tabela, cadastrar_usuario
+from banco import criar_tabela, cadastrar_usuario, adicionar_avaliacao
 
 criar_tabela()
 
@@ -16,3 +16,23 @@ for usuario, senha in usuarios_teste:
         print(f"Usuário '{usuario}' criado com sucesso!")
     else:
         print(f"Usuário '{usuario}' já existe no banco.")
+
+teste_avaliacao = [
+    ("Anna", "0195153448", 3),
+    ("Anna", "0374157065", 4),
+    ("Anna", "0375705856", 2),
+    ("Anna", "0440241413", 5),
+    ("Anna", "1401201172", 2),
+    ("Anna", "8486433193", 1),
+    ("Anna", "1841721522", 3),
+    ("Anna", "0553582909", 4),
+    ("Anna", "0316769487", 2),
+    ("Anna", "0446608653", 1)
+]
+
+for usuario, ISBN, nota in teste_avaliacao:
+    try:
+        adicionar_avaliacao(usuario,ISBN,nota)
+        print("Avaliacao cadastrada: {usuario}, {ISBN}, {nota}")
+    except Exception as e:
+        print(f"Erro!")
