cb2b50b8424b8e9d4904cf5409364331a857cea5
AnnaCSantos <annabeatrizsantos@alunos.utfpr.edu.br>
2026-06-21 18:11:58 -0300

    Atualizacao de avaliacoes testes

--- Projeto/ini_banco.py
+++ Projeto/ini_banco.py
@@ -1,5 +1,5 @@
 # seed.py
-from banco import criar_tabela, cadastrar_usuario, adicionar_avaliacao
+from banco import criar_tabela, cadastrar_usuario, adicionar_avaliacao, obter_user_id
 
 criar_tabela()
 
@@ -16,23 +16,23 @@ for usuario, senha in usuarios_teste:
         print(f"Usuário '{usuario}' criado com sucesso!")
     else:
         print(f"Usuário '{usuario}' já existe no banco.")
-
 teste_avaliacao = [
-    ("Anna", "0195153448", 3),
-    ("Anna", "0374157065", 4),
-    ("Anna", "0375705856", 2),
-    ("Anna", "0440241413", 5),
+    ("Anna", "0195153448", 8),
+    ("Anna", "0374157065", 7),
+    ("Anna", "0375705856", 5),
+    ("Anna", "0440241413", 10),
     ("Anna", "1401201172", 2),
-    ("Anna", "8486433193", 1),
+    ("Anna", "8486433193", 6),
     ("Anna", "1841721522", 3),
-    ("Anna", "0553582909", 4),
-    ("Anna", "0316769487", 2),
+    ("Anna", "0553582909", 10),
+    ("Anna", "0316769487", 9),
     ("Anna", "0446608653", 1)
 ]
 
 for usuario, ISBN, nota in teste_avaliacao:
     try:
-        adicionar_avaliacao(usuario,ISBN,nota)
-        print("Avaliacao cadastrada: {usuario}, {ISBN}, {nota}")
+        usuario_id = obter_user_id(usuario)
+        adicionar_avaliacao(usuario_id,ISBN,nota)
+        print(f"Avaliacao cadastrada: {usuario}, {ISBN}, {nota}")
     except Exception as e:
         print(f"Erro!")
